3ec0570c0f69b65529ca7c9ee043fc5e493e6dbd
James Elías Sigurðarson <jamiees2@gmail.com>
2019-01-30 11:16:27 +0000

    add polygon demo testing screenbbox

--- demo/index.js
+++ demo/index.js
@@ -13,6 +13,11 @@ const Component = () => {
     cy: "147.5",
     r: "92.5"
   }
+  const starProps = {
+    id:"Star-1",
+    fill: "#FFF681",
+    points:"384 349 328.748186 378.047597 339.300344 316.523799 294.600687 272.952403 356.374093 263.976201 384 208 411.625907 263.976201 473.399313 272.952403 428.699656 316.523799 439.251814 378.047597"
+  }
   return (
     <div>
       <h1>Demo!</h1>
@@ -31,7 +36,7 @@ const Component = () => {
           <g id="journocode" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
               <g id="example">
                   <TooltipsterSVG rootType="circle" rootProps={ovalProps} content="I am a circle!" side="right"></TooltipsterSVG>
-                  <polygon id="Star-1" fill="#FFF681" points="384 349 328.748186 378.047597 339.300344 316.523799 294.600687 272.952403 356.374093 263.976201 384 208 411.625907 263.976201 473.399313 272.952403 428.699656 316.523799 439.251814 378.047597" class="tooltip" title="<strong>Stars for the win</strong><br>I am a star!"></polygon> 
+                  <TooltipsterSVG rootType="polygon" rootProps={starProps} content={<><strong>Stars for the win</strong><br />I am a star!</>}></TooltipsterSVG> 
                   <polygon id="Triangle-1" fill="#A9FFEC" points="619 72 717 223 521 223" class="tooltip" title="<p style='color:#F85D00'>Hell yeah!</p>"></polygon> 
               </g> 
           </g> 
